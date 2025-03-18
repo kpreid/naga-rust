d99c4e4c158b599924bd033cc7bdaa246114fee4
Kevin Reid <kpreid@switchb.org>
2025-03-17 18:31:32 -0700

    Tests of constants.

--- tests/interop.rs
+++ tests/interop.rs
@@ -5,6 +5,23 @@ use naga_rust::rt::Vec2;
 use naga_rust::wgsl;
 
 #[test]
+fn global_constant() {
+    wgsl!("const X: f32 = 1234.0;");
+    assert_eq!(X, 1234.0);
+}
+
+#[test]
+fn local_constant() {
+    wgsl!(
+        r"fn foo() -> f32 {
+            const X: f32 = 1234.0;
+            return X;
+        }"
+    );
+    assert_eq!(foo(), 1234.0);
+}
+
+#[test]
 fn scalar_arithmetic() {
     wgsl!(
         r"fn add_one(x: i32) -> i32 {
