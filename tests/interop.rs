917d9882f929f6d28247bdd634ef6028c8669083
Kevin Reid <kpreid@switchb.org>
2025-03-20 13:16:10 -0700

    Disabled test for atomics.

--- tests/interop.rs
+++ tests/interop.rs
@@ -116,6 +116,29 @@ fn logical_ops() {
     }
 }
 
+// TODO: implement atomics
+// #[test]
+// fn atomics() {
+//     wgsl!(
+//         global_struct = Globals,
+//         r"
+//         @group(0) @binding(0)
+//         var<storage, read_write> storage_atomic_scalar: atomic<u32>;
+//
+//         fn atomic_ops() {
+//             atomicAdd(&storage_atomic_scalar, 10u);
+//             atomicAnd(&storage_atomic_scalar, 3u);
+//         }
+//         "
+//     );
+//
+//     let globals = Globals {
+//         x: core::sync::atomic::AtomicU32::new(2);
+//     };
+//     globals.atomic_ops();
+//     assert_eq!(globals.x.load(core::sync::atomic::Ordering::Relaxed), 36);
+// }
+
 #[test]
 fn array_access_fixed() {
     wgsl!(
