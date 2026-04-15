ccd269231b541e7b9541d96148102d4f1f11c5af
Kevin Reid <kpreid@switchb.org>
2026-04-15 09:39:12 -0700

    Define textures in terms of a set of predefined `Texture*` structs.
    This is a lot more boilerplate for us, but it has advantages:
    
    * Users no longer have to implement all the getters/queries, and
      therefore can’t do it in an inconsistent or overly expensive way.
    * Frees users of the obligation to pass us a reference to a texture
      object; now they only need to pass a reference to the texture data
      storage, which is a lot more likely to already exist in some form.
      (We should still get rid of the reference entirely somehow.)

--- embed/tests/interop/textures.rs
+++ embed/tests/interop/textures.rs
@@ -1,47 +1,86 @@
+use core::cell::RefCell;
+use core::marker::PhantomData;
 use core::num::NonZeroU32;
 
-use naga_rust_embed::rt::{self, Vec2, Vec4};
+use naga_rust_embed::rt::{self, Scalar, Vec2, Vec3, Vec4, texture::ONE};
 use naga_rust_embed::wgsl;
 
-#[test]
-fn texture_query_and_load() {
-    struct MyTexture;
-    impl rt::Texture for MyTexture {
-        type Dimensions = Vec2<u32>;
-        type Coordinates = Vec2<i32>;
-        type Scalar = f32;
+// -------------------------------------------------------------------------------------------------
 
-        fn dimensions(&self, _mip_level: i32) -> Self::Dimensions {
-            Vec2::new(100, 100)
-        }
-
-        fn array_layers(&self) -> NonZeroU32 {
-            NonZeroU32::MIN
-        }
-
-        fn mip_levels(&self) -> NonZeroU32 {
-            NonZeroU32::MIN
-        }
-
-        fn samples(&self) -> NonZeroU32 {
-            NonZeroU32::MIN
-        }
-
-        fn load(
-            &self,
-            coordinates: Self::Coordinates,
-            _array_layer: i32,
-            _sample: i32,
-            mip_level: i32,
-        ) -> Vec4<f32> {
-            assert_eq!(coordinates, Vec2::new(10, 20));
-            assert_eq!(mip_level, 0);
-            Vec4::new(1.0, 2.0, 3.0, 4.0)
+struct MockTextureStorage<C, F: ?Sized> {
+    _phantom: PhantomData<fn(C)>,
+    function: RefCell<F>,
+}
+impl<C, F> MockTextureStorage<C, F>
+where
+    C: Copy + 'static,
+    F: FnMut(C, i32, i32, i32) -> Vec4<f32>,
+{
+    fn new(function: F) -> Self {
+        Self {
+            _phantom: PhantomData,
+            function: RefCell::new(function),
         }
     }
+}
+impl<C, F> rt::texture::Read for MockTextureStorage<C, F>
+where
+    C: Copy + 'static,
+    F: FnMut(C, i32, i32, i32) -> Vec4<f32>,
+{
+    type Coordinates = C;
+    type Component = f32;
 
-    // TODO: for full coverage, we need many different texture binding types and their corresponding
-    // call overloads.
+    fn read_texel(
+        &self,
+        coordinates: Self::Coordinates,
+        array_layer: i32,
+        sample: i32,
+        mip_level: i32,
+    ) -> Vec4<f32> {
+        (self.function.borrow_mut())(coordinates, array_layer, sample, mip_level)
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+#[test]
+fn query_and_load_1d() {
+    wgsl!(
+        resource_struct = Resources,
+        r"
+        @group(0) @binding(0) var my_texture: texture_1d<f32>;
+        fn dimensions() -> u32 {
+            return textureDimensions(my_texture, 0);
+        }
+        fn load(position: i32) -> vec4f {
+            return textureLoad(my_texture, position, 0);
+        }"
+    );
+
+    let mut call_count = 0;
+
+    let ts = MockTextureStorage::new(|coordinates, _a, _s, mip_level| {
+        assert_eq!(coordinates, Scalar::new(10));
+        assert_eq!(mip_level, 0);
+        call_count += 1;
+        Vec4::new(1.0, 2.0, 3.0, 4.0)
+    });
+
+    let res = Resources {
+        my_texture: rt::Texture1d {
+            dimensions: Scalar::new(NonZeroU32::new(100).unwrap()),
+            mip_levels: NonZeroU32::new(1).unwrap(),
+            data: &ts,
+        },
+    };
+    assert_eq!(res.dimensions(), 100);
+    assert_eq!(res.load(Scalar::new(10)), Vec4::new(1.0, 2.0, 3.0, 4.0));
+    assert_eq!(call_count, 1);
+}
+
+#[test]
+fn query_and_load_2d() {
     wgsl!(
         resource_struct = Resources,
         r"
@@ -54,12 +93,154 @@ fn texture_query_and_load() {
         }"
     );
 
+    let mut call_count = 0;
+
+    let ts = MockTextureStorage::new(|coordinates, _a, _s, mip_level| {
+        assert_eq!(coordinates, Vec2::new(10, 20));
+        assert_eq!(mip_level, 0);
+        call_count += 1;
+        Vec4::new(1.0, 2.0, 3.0, 4.0)
+    });
+
     let res = Resources {
-        my_texture: &MyTexture,
+        my_texture: rt::Texture2d {
+            dimensions: Vec2::splat(NonZeroU32::new(100).unwrap()),
+            mip_levels: NonZeroU32::new(1).unwrap(),
+            data: &ts,
+        },
     };
     assert_eq!(res.dimensions(), Vec2::new(100u32, 100));
     assert_eq!(res.load(Vec2::new(10, 20)), Vec4::new(1.0, 2.0, 3.0, 4.0));
+    assert_eq!(call_count, 1);
 }
+
+#[test]
+fn query_and_load_3d() {
+    wgsl!(
+        resource_struct = Resources,
+        r"
+        @group(0) @binding(0) var my_texture: texture_3d<f32>;
+        fn dimensions() -> vec3u {
+            return textureDimensions(my_texture, 0);
+        }
+        fn load(position: vec3i) -> vec4f {
+            return textureLoad(my_texture, position, 0);
+        }"
+    );
+
+    let mut call_count = 0;
+
+    let ts = MockTextureStorage::new(|coordinates, _a, _s, mip_level| {
+        assert_eq!(coordinates, Vec3::new(10, 20, 30));
+        assert_eq!(mip_level, 0);
+        call_count += 1;
+        Vec4::new(1.0, 2.0, 3.0, 4.0)
+    });
+
+    let res = Resources {
+        my_texture: rt::Texture3d {
+            dimensions: Vec3::splat(NonZeroU32::new(100).unwrap()),
+            mip_levels: NonZeroU32::new(1).unwrap(),
+            data: &ts,
+        },
+    };
+    assert_eq!(res.dimensions(), Vec3::new(100u32, 100, 100));
+    assert_eq!(
+        res.load(Vec3::new(10, 20, 30)),
+        Vec4::new(1.0, 2.0, 3.0, 4.0)
+    );
+    assert_eq!(call_count, 1);
+}
+
+// TODO: test all texture types
+
+// -------------------------------------------------------------------------------------------------
+
+#[test]
+fn mip_level_dimensions() {
+    wgsl!(
+        resource_struct = Resources,
+        r"
+        @group(0) @binding(0) var texture1d: texture_1d<f32>;
+        @group(0) @binding(0) var texture2d: texture_2d<f32>;
+        @group(0) @binding(0) var texture3d: texture_3d<f32>;
+        struct Output {
+            one: u32,
+            two: vec2u,
+            three: vec3u,
+        }
+        fn dimensions(mip_level: i32) -> Output {
+            return Output(
+                textureDimensions(texture1d, mip_level),
+                textureDimensions(texture2d, mip_level),
+                textureDimensions(texture3d, mip_level),
+            );
+        }
+        "
+    );
+
+    let res = Resources {
+        texture1d: rt::Texture1d {
+            dimensions: Scalar::new(NonZeroU32::new(15).unwrap()),
+            mip_levels: NonZeroU32::new(1).unwrap(),
+            data: &rt::texture::Constant::new(Vec4::splat(0.0)),
+        },
+        texture2d: rt::Texture2d {
+            dimensions: Vec2::new(ONE, NonZeroU32::new(15).unwrap()),
+            mip_levels: NonZeroU32::new(1).unwrap(),
+            data: &rt::texture::Constant::new(Vec4::splat(0.0)),
+        },
+        texture3d: rt::Texture3d {
+            dimensions: Vec3::new(
+                ONE,
+                NonZeroU32::new(15).unwrap(),
+                NonZeroU32::new(16).unwrap(),
+            ),
+            mip_levels: NonZeroU32::new(1).unwrap(),
+            data: &rt::texture::Constant::new(Vec4::splat(0.0)),
+        },
+    };
+    pretty_assertions::assert_eq!(
+        [
+            res.dimensions(0),
+            res.dimensions(1),
+            res.dimensions(2),
+            res.dimensions(3),
+            res.dimensions(4),
+        ],
+        [
+            Output {
+                one: 15,
+                two: Vec2::new(1, 15),
+                three: Vec3::new(1, 15, 16),
+            },
+            Output {
+                one: 7,
+                two: Vec2::new(1, 7),
+                three: Vec3::new(1, 7, 8),
+            },
+            Output {
+                one: 3,
+                two: Vec2::new(1, 3),
+                three: Vec3::new(1, 3, 4),
+            },
+            Output {
+                one: 1,
+                two: Vec2::new(1, 1),
+                three: Vec3::new(1, 1, 2),
+            },
+            // out of range for all but the 3D example; indeterminate per spec
+            // <<https://www.w3.org/TR/2026/CRD-WGSL-20260507/#texturedimensions>
+            Output {
+                one: 1,
+                two: Vec2::new(1, 1),
+                three: Vec3::new(1, 1, 1),
+            },
+        ]
+    );
+}
+
+// -------------------------------------------------------------------------------------------------
 
 /// We don’t implement texture sampling yet, but shaders that mentions samplers should still be
 /// compilable.
--- rt/src/texture.rs
+++ rt/src/texture.rs
@@ -1,119 +1,413 @@
 use core::marker::PhantomData;
 use core::num::NonZeroU32;
 
-use crate::{Vec2, Vec3, Vec4};
+use crate::{Scalar, Vec2, Vec3, Vec4};
+
+// -------------------------------------------------------------------------------------------------
 
 /// Texture sampler (placeholder).
 ///
 /// Use this type to satisfy a sampler binding in a resource struct.
 pub struct Sampler;
 
-// TODO: Consider, instead of putting all the metadata as methods on the Txture trait,
-// defining a struct with the metadata. This ensures consistency (particularly for things like
-// per-mip-level dimensions) and avoids ever using dynamic dispatch, at the price of possibly
-// duplicating some integers.
+/// The number 1, as a [`NonZeroU32`].
+///
+/// Convenience alias because textures heavily use non-zero sizes.
+pub const ONE: NonZeroU32 = NonZeroU32::MIN;
 
-/// Implement this trait to provide a texture to the shader code.
-// TODO: This will need expansion to handle depth textures
-pub trait Texture {
-    /// Type of the dimensions of the texture.
-    /// Should be a [`Scalar`][crate::Scalar], [`Vec2`][crate::Vec2], or [`Vec3`][crate::Vec3]
-    /// of `u32`.
-    type Dimensions: Copy + 'static;
+// -------------------------------------------------------------------------------------------------
 
-    /// Type of a point within the texture.
-    /// Should be a [`Scalar`][crate::Scalar], [`Vec2`][crate::Vec2], or [`Vec3`][crate::Vec3]
-    /// of `i32`.
+/// 1-dimensional texture object.
+///
+/// Use this type to satisfy a texture binding in a resource struct,
+/// when the binding has WGSL type `texture_1d`.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct Texture1d<T: ?Sized> {
+    pub dimensions: Scalar<NonZeroU32>,
+    pub mip_levels: NonZeroU32,
+    pub data: T,
+}
+
+/// 2-dimensional texture object.
+///
+/// Use this type to satisfy a texture binding in a resource struct,
+/// when the binding has WGSL type `texture_2d` or `texture_external`.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct Texture2d<T: ?Sized> {
+    pub dimensions: Vec2<NonZeroU32>,
+    pub mip_levels: NonZeroU32,
+    pub data: T,
+}
+
+/// 2-dimensional array texture object.
+///
+/// Use this type to satisfy a texture binding in a resource struct,
+/// when the binding has WGSL type `texture_2d_array`.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct Texture2dArray<T: ?Sized> {
+    pub dimensions: Vec2<NonZeroU32>,
+    pub array_layers: NonZeroU32,
+    pub mip_levels: NonZeroU32,
+    pub data: T,
+}
+
+/// 3-dimensional texture object.
+///
+/// Use this type to satisfy a texture binding in a resource struct,
+/// when the binding has WGSL type `texture_3d`.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct Texture3d<T: ?Sized> {
+    pub dimensions: Vec3<NonZeroU32>,
+    pub mip_levels: NonZeroU32,
+    pub data: T,
+}
+
+/// Cubemap texture object.
+///
+/// Use this type to satisfy a texture binding in a resource struct,
+/// when the binding has WGSL type `texture_cube`.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct TextureCube<T: ?Sized> {
+    pub dimensions: Vec2<NonZeroU32>,
+    pub mip_levels: NonZeroU32,
+    pub data: T,
+}
+
+/// Cubemap array texture object.
+///
+/// Use this type to satisfy a texture binding in a resource struct,
+/// when the binding has WGSL type `texture_cube_array`.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct TextureCubeArray<T: ?Sized> {
+    pub dimensions: Vec2<NonZeroU32>,
+    pub array_layers: NonZeroU32,
+    pub mip_levels: NonZeroU32,
+    pub data: T,
+}
+
+/// Multisampled texture object.
+///
+/// Use this type to satisfy a texture binding in a resource struct,
+/// when the binding has WGSL type `texture_multisampled_2d`.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct TextureMultisampled2d<T: ?Sized> {
+    pub dimensions: Vec2<NonZeroU32>,
+    pub samples: NonZeroU32,
+    pub data: T,
+}
+
+// -------------------------------------------------------------------------------------------------
+
+// TODO: we want to have these convenient constructors but without a requirement for borrowing.
+// We could solve that by using `Box` instead of `&`, but it would be better to work on a general
+// mechanism to avoid `dyn`.
+
+impl<'d, C: Component> Texture1d<&'d dyn Read<Coordinates = Scalar<i32>, Component = C>> {
+    pub fn one_texel(texel: &'d Constant<Scalar<i32>, C>) -> Self {
+        Self {
+            dimensions: Scalar::new(ONE),
+            mip_levels: ONE,
+            data: texel,
+        }
+    }
+}
+
+impl<'d, C: Component> Texture2d<&'d dyn Read<Coordinates = Vec2<i32>, Component = C>> {
+    pub fn one_texel(texel: &'d Constant<Vec2<i32>, C>) -> Self {
+        Self {
+            dimensions: Vec2::splat(ONE),
+            mip_levels: ONE,
+            data: texel,
+        }
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/// Reads one texel from a texture.
+///
+/// Implement this trait and then put the implementation in a [`Texture1d`], [`Texture2d`], etc.
+/// to provide a texture to your shader code.
+//
+// TODO: Usage example.
+pub trait Read {
     type Coordinates: Copy + 'static;
-
-    /// Numeric type exposed to the shader.
-    ///
-    /// Should be one of [`u32`], [`i32`], or [`f32`], and must match the parameter of the
-    /// texture type in the shader.
-    type Scalar;
-
-    /// Returns the dimensions of the texture.
-    fn dimensions(&self, mip_level: i32) -> Self::Dimensions;
-
-    /// Returns the count of array layers of the texture.
-    fn array_layers(&self) -> NonZeroU32;
-
-    /// Returns the count of mip levels of the texture.
-    fn mip_levels(&self) -> NonZeroU32;
-
-    /// Returns the count of samples of the texture.
-    fn samples(&self) -> NonZeroU32;
+    type Component: Component;
 
     /// Loads a single texel from the texture.
     ///
-    /// If the coordinates are out of bounds, do not panic, but perform one of the behaviors
-    /// specified in <https://www.w3.org/TR/WGSL/#textureload>.
-    fn load(
+    /// If the coordinates are out of bounds, the implementation should not panic,
+    /// but perform one of the behaviors specified in <https://www.w3.org/TR/WGSL/#textureload>.
+    fn read_texel(
         &self,
         coordinates: Self::Coordinates,
         array_layer: i32,
         sample: i32,
         mip_level: i32,
-    ) -> Vec4<Self::Scalar>;
+    ) -> Vec4<Self::Component>;
 }
 
-/// A [`Texture`] whose texels all have a single value.
-pub struct ConstantTexture<D: Copy + 'static, C: Copy + 'static, S> {
-    pub dimensions: D,
-    pub coordinates: PhantomData<fn(C)>,
-    pub texel: Vec4<S>,
-}
+/// Information about a texture.
+///
+/// This trait is implemented by all the texture types like [`Texture1d`], [`Texture2d`], etc.
+pub trait Query {
+    /// Type of the dimensions of the texture.
+    /// Should be a [`Scalar`], [`Vec2`], or [`Vec3`] of [`NonZeroU32`].
+    type Dimensions: Dimensions;
 
-impl<S: Copy> ConstantTexture<Vec2<u32>, Vec2<i32>, S> {
-    pub const fn new_2d(dimensions: Vec2<u32>, texel: Vec4<S>) -> Self {
-        Self {
-            dimensions,
-            coordinates: PhantomData,
-            texel,
-        }
-    }
-}
+    /// Numeric type exposed to the shader.
+    ///
+    /// Should be one of [`u32`], [`i32`], or [`f32`], and must match the parameter of the
+    /// texture type in the shader.
+    type Component: Component;
 
-impl<S: Copy> ConstantTexture<Vec3<u32>, Vec3<i32>, S> {
-    pub const fn new_3d(dimensions: Vec3<u32>, texel: Vec4<S>) -> Self {
-        Self {
-            dimensions,
-            coordinates: PhantomData,
-            texel,
-        }
-    }
-}
+    /// Returns the dimensions of mip level 0 of the texture.
+    fn base_dimensions(&self) -> Self::Dimensions;
 
-impl<D: Copy + 'static, C: Copy + 'static, S: Copy> Texture for ConstantTexture<D, C, S> {
-    type Dimensions = D;
-
-    type Coordinates = C;
-
-    type Scalar = S;
-
-    fn dimensions(&self, _mip_level: i32) -> Self::Dimensions {
-        self.dimensions
-    }
-
+    /// Returns the count of array layers of the texture.
+    ///
+    /// For non-array textures, returns 1.
     fn array_layers(&self) -> NonZeroU32 {
-        NonZeroU32::MIN
+        ONE
     }
 
+    /// Returns the count of mip levels of the texture.
     fn mip_levels(&self) -> NonZeroU32 {
-        NonZeroU32::MIN
+        ONE
     }
 
+    /// Returns the count of samples of the texture.
+    ///
+    /// For non-multisampled textures, returns 1.
     fn samples(&self) -> NonZeroU32 {
-        NonZeroU32::MIN
+        ONE
     }
+}
 
-    fn load(
+/// Types which can be components of texels.
+pub trait Component: Copy + 'static {}
+impl Component for u32 {}
+impl Component for i32 {}
+impl Component for f32 {}
+
+// -------------------------------------------------------------------------------------------------
+
+macro_rules! impl_read_forwarder_struct {
+    ($texture_type:ident) => {
+        impl<T: ?Sized + Read> Read for $texture_type<T> {
+            type Coordinates = T::Coordinates;
+            type Component = T::Component;
+
+            fn read_texel(
+                &self,
+                coordinates: Self::Coordinates,
+                array_layer: i32,
+                sample: i32,
+                mip_level: i32,
+            ) -> Vec4<Self::Component> {
+                self.data
+                    .read_texel(coordinates, array_layer, sample, mip_level)
+            }
+        }
+    };
+}
+
+impl_read_forwarder_struct!(Texture1d);
+impl_read_forwarder_struct!(Texture2d);
+impl_read_forwarder_struct!(Texture2dArray);
+impl_read_forwarder_struct!(Texture3d);
+impl_read_forwarder_struct!(TextureCube);
+impl_read_forwarder_struct!(TextureCubeArray);
+impl_read_forwarder_struct!(TextureMultisampled2d);
+
+macro_rules! impl_read_forwarder_deref {
+    ($($ty:tt)*) => {
+        impl<T: ?Sized + Read> Read for $($ty)* {
+            type Coordinates = T::Coordinates;
+            type Component = T::Component;
+
+            fn read_texel(
+                &self,
+                coordinates: Self::Coordinates,
+                array_layer: i32,
+                sample: i32,
+                mip_level: i32,
+            ) -> Vec4<Self::Component> {
+                T::read_texel(&**self, coordinates, array_layer, sample, mip_level)
+            }
+        }
+    };
+}
+
+impl_read_forwarder_deref!(&T);
+impl_read_forwarder_deref!(&mut T);
+#[cfg(feature = "alloc")]
+impl_read_forwarder_deref!(alloc::boxed::Box<T>);
+#[cfg(feature = "alloc")]
+impl_read_forwarder_deref!(alloc::rc::Rc<T>);
+#[cfg(feature = "alloc")]
+impl_read_forwarder_deref!(alloc::sync::Arc<T>);
+
+// -------------------------------------------------------------------------------------------------
+
+macro_rules! query_common {
+    () => {
+        type Component = T::Component;
+        fn base_dimensions(&self) -> Self::Dimensions {
+            self.dimensions
+        }
+    };
+}
+
+macro_rules! query_mip {
+    () => {
+        fn mip_levels(&self) -> NonZeroU32 {
+            self.mip_levels
+        }
+    };
+}
+
+impl<T: ?Sized + Read> Query for Texture1d<T> {
+    type Dimensions = Scalar<NonZeroU32>;
+    query_common!();
+    query_mip!();
+}
+
+impl<T: ?Sized + Read> Query for Texture2d<T> {
+    type Dimensions = Vec2<NonZeroU32>;
+    query_common!();
+    query_mip!();
+}
+
+impl<T: ?Sized + Read> Query for Texture2dArray<T> {
+    type Dimensions = Vec2<NonZeroU32>;
+    query_common!();
+    query_mip!();
+    fn array_layers(&self) -> NonZeroU32 {
+        self.array_layers
+    }
+}
+
+impl<T: ?Sized + Read> Query for Texture3d<T> {
+    type Dimensions = Vec3<NonZeroU32>;
+    query_common!();
+    query_mip!();
+}
+
+impl<T: ?Sized + Read> Query for TextureCube<T> {
+    type Dimensions = Vec2<NonZeroU32>;
+    query_common!();
+    query_mip!();
+}
+
+impl<T: ?Sized + Read> Query for TextureCubeArray<T> {
+    type Dimensions = Vec2<NonZeroU32>;
+    query_common!();
+    query_mip!();
+    fn array_layers(&self) -> NonZeroU32 {
+        self.array_layers
+    }
+}
+
+impl<T: ?Sized + Read> Query for TextureMultisampled2d<T> {
+    type Dimensions = Vec2<NonZeroU32>;
+    query_common!();
+    fn samples(&self) -> NonZeroU32 {
+        self.samples
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/// Generic texture data container for textures whose texels are all identical.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)] // TODO: derives have bounds on C
+pub struct Constant<C, T> {
+    pub texel: Vec4<T>,
+    _phantom: PhantomData<fn(C)>,
+}
+
+impl<C: Copy + 'static, T: Component> Constant<C, T> {
+    pub const fn new(texel: Vec4<T>) -> Self {
+        Self {
+            texel,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<C: Copy + 'static, T: Component> Read for Constant<C, T> {
+    type Coordinates = C;
+    type Component = T;
+
+    fn read_texel(
         &self,
         _coordinates: Self::Coordinates,
         _array_layer: i32,
         _sample: i32,
         _mip_level: i32,
-    ) -> Vec4<Self::Scalar> {
+    ) -> Vec4<Self::Component> {
         self.texel
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+/// Computes the dimensions of a specific mip level of a texture.
+///
+/// The result is meaningless if the mip level is out of bounds.
+//
+// Design note: The result is `u32`, not `NonZeroU32`, for the sake of the shader code which does
+// not have `NonZero` types.
+pub fn dimensions<Q>(texture: &Q, mip_level: i32) -> <Q::Dimensions as Dimensions>::Plain
+where
+    Q: ?Sized + Query,
+{
+    Dimensions::at_mip_level(texture.base_dimensions(), mip_level as u32)
+}
+
+use dim::Dimensions;
+mod dim {
+    use super::*;
+
+    /// Semi-private trait implemented for vectors that can describe texture dimensions.
+    /// Helper for [`dimensions()`].
+    pub trait Dimensions: Copy + 'static {
+        /// The vector with `u32` components instead of `NonZeroU32`.
+        type Plain;
+
+        fn at_mip_level(self, mip_level: u32) -> Self::Plain;
+    }
+
+    impl Dimensions for Scalar<NonZeroU32> {
+        type Plain = Scalar<u32>;
+        fn at_mip_level(self, mip_level: u32) -> Self::Plain {
+            Scalar::new(mip_divide_size(self.0, mip_level))
+        }
+    }
+    impl Dimensions for Vec2<NonZeroU32> {
+        type Plain = Vec2<u32>;
+        fn at_mip_level(self, mip_level: u32) -> Self::Plain {
+            Vec2::new(
+                mip_divide_size(self.x, mip_level),
+                mip_divide_size(self.y, mip_level),
+            )
+        }
+    }
+    impl Dimensions for Vec3<NonZeroU32> {
+        type Plain = Vec3<u32>;
+        fn at_mip_level(self, mip_level: u32) -> Self::Plain {
+            Vec3::new(
+                mip_divide_size(self.x, mip_level),
+                mip_divide_size(self.y, mip_level),
+                mip_divide_size(self.z, mip_level),
+            )
+        }
+    }
+
+    /// Computes one component of the size of a given mip level of a texture,
+    /// given the size at level 0.
+    fn mip_divide_size(size: NonZeroU32, mip_level: u32) -> u32 {
+        // Per <https://www.w3.org/TR/2026/CRD-webgpu-20260507/#abstract-opdef-logical-miplevel-specific-texture-extent>
+        (size.get() >> mip_level).max(1)
     }
 }
