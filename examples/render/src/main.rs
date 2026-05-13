8b208953769eea1198961b258e6e92e4da7f3ea6
Kevin Reid <kpreid@switchb.org>
2026-05-12 21:18:11 -0700

    Use `wgpu` instead of `softbuffer` for presenting the render example.
    While there is some value in saying “see, we ran a shader and there
    was no GPU API involved”, the `wgpu` implementation has better
    throughput (at least on macOS) and I think that it’s more important
    to reduce costs unrelated to the shader than it is to maintain that
    purity.
    
    Furthermore, I am thinking of extending this example to display
    split-screen CPU and GPU execution, and also having tests that
    validate the CPU shader implementation by comparing its output to
    the GPU implementation. Both of those would require `wgpu`.

--- examples/render/src/main.rs
+++ examples/render/src/main.rs
@@ -1,7 +1,8 @@
 //! Example of using [`naga_rust_embed`] to execute a fragment shader on the CPU.
 //! This is not very efficient, but it works.
 
-use std::num::NonZeroU32;
+#![allow(clippy::pedantic)]
+
 use std::sync::Arc;
 use std::time::Instant;
 
@@ -21,9 +22,13 @@ naga_rust_embed::include_wgsl_mr!(resource_struct = Shader, "src/shader.wgsl");
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
+    let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::new_with_display_handle_from_env(
+        Box::new(event_loop.owned_display_handle()),
+    ));
     event_loop.set_control_flow(ControlFlow::Poll);
     event_loop
         .run_app(&mut App {
+            instance,
             active: None,
             start_time: Instant::now(),
         })
@@ -32,7 +37,7 @@ fn main() {
 
 // -------------------------------------------------------------------------------------------------
 
-fn run_fragment_shader(time: f32, buffer: &mut [u32], size: PhysicalSize<u32>) {
+fn run_fragment_shader(time: f32, buffer: &mut [[u8; 4]], size: PhysicalSize<u32>) {
     // Run in parallel to take advantage of all CPU cores.
     //
     // We parallelize rows but run each row serially to ensure that the inner loop has no
@@ -58,11 +63,9 @@ fn run_fragment_shader(time: f32, buffer: &mut [u32], size: PhysicalSize<u32>) {
                 .main(fragment_position);
 
                 // In a real application this should be sRGB encoding.
-                let v = (result * rt::Scalar(255.0))
-                    .cast_elem_as_u32()
-                    .map(|c| c.clamp(0, 255));
+                let v = (result * rt::Scalar(255.0)).map(|c: f32| c as u8);
 
-                *pixel = v.z | (v.y << 8) | (v.x << 16);
+                *pixel = [v.z, v.y, v.x, 0xFF];
             }
         });
 }
@@ -70,12 +73,16 @@ fn run_fragment_shader(time: f32, buffer: &mut [u32], size: PhysicalSize<u32>) {
 // -------------------------------------------------------------------------------------------------
 
 struct App {
+    instance: wgpu::Instance,
     active: Option<Active>,
     start_time: Instant,
 }
 struct Active {
     window: Arc<Window>,
-    surface: softbuffer::Surface<Arc<Window>, Arc<Window>>,
+    surface: wgpu::Surface<'static>,
+    cpu_buffer: Vec<[u8; 4]>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
 }
 
 impl ApplicationHandler for App {
@@ -90,10 +97,29 @@ impl ApplicationHandler for App {
                     )
                     .unwrap(),
             );
-            let context = softbuffer::Context::new(window.clone()).unwrap();
-            let surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
+            let surface = self.instance.create_surface(window.clone()).unwrap();
+            let adapter = pollster::block_on(wgpu::util::initialize_adapter_from_env_or_default(
+                &self.instance,
+                Some(&surface),
+            ))
+            .unwrap();
+            let (device, queue) =
+                pollster::block_on(adapter.request_device(&wgpu::DeviceDescriptor {
+                    required_limits: wgpu::Limits::default().using_resolution(adapter.limits()),
+                    ..Default::default()
+                }))
+                .unwrap();
 
-            self.active = Some(Active { window, surface });
+            let active = Active {
+                window,
+                surface,
+                device,
+                queue,
+                cpu_buffer: Vec::new(),
+            };
+            active.configure_surface();
+
+            self.active = Some(active);
         }
     }
 
@@ -106,27 +132,103 @@ impl ApplicationHandler for App {
             WindowEvent::CloseRequested => {
                 event_loop.exit();
             }
+            WindowEvent::Resized(_) => {
+                if let Some(active) = &self.active {
+                    active.configure_surface();
+                }
+            }
             WindowEvent::RedrawRequested => {
-                if let Some(Active { window, surface }) = &mut self.active {
+                if let Some(active) = &mut self.active {
                     let time = self.start_time.elapsed().as_secs_f32();
-                    let size = window.inner_size();
-                    surface
-                        .resize(
-                            NonZeroU32::new(size.width).unwrap(),
-                            NonZeroU32::new(size.height).unwrap(),
-                        )
-                        .unwrap();
+                    match active.surface.get_current_texture() {
+                        wgpu::CurrentSurfaceTexture::Success(surface_texture)
+                        | wgpu::CurrentSurfaceTexture::Suboptimal(surface_texture) => {
+                            active.render(time, surface_texture)
+                        }
+                        wgpu::CurrentSurfaceTexture::Occluded => {}
+                        //t => panic!("{t:?}"),
+                        wgpu::CurrentSurfaceTexture::Timeout => {}
+                        wgpu::CurrentSurfaceTexture::Outdated => {
+                            active.configure_surface();
+                        }
+                        wgpu::CurrentSurfaceTexture::Lost => {
+                            todo!("recreating device not implemented")
+                        }
+                        wgpu::CurrentSurfaceTexture::Validation => {
+                            unreachable!("we are not catching errors")
+                        }
+                    }
+                }
 
-                    let mut buffer = surface.buffer_mut().unwrap();
-                    run_fragment_shader(time, &mut buffer, size);
-
-                    buffer.present().unwrap();
-
-                    // Continuous animation.
+                // Continuous animation.
+                if let Some(Active { window, .. }) = &self.active {
                     window.request_redraw();
                 }
             }
             _ => (),
         }
+    }
+}
+
+impl Active {
+    fn configure_surface(&self) {
+        let size = self.window.inner_size();
+        self.surface.configure(
+            &self.device,
+            &wgpu::SurfaceConfiguration {
+                usage: wgpu::TextureUsages::COPY_DST,
+                // guaranteed to work per https://docs.rs/wgpu/29.0.3/wgpu/type.SurfaceConfiguration.html#structfield.format
+                format: wgpu::TextureFormat::Bgra8Unorm,
+                width: size.width,
+                height: size.height,
+                present_mode: wgpu::PresentMode::AutoVsync,
+                desired_maximum_frame_latency: 3,
+                alpha_mode: wgpu::CompositeAlphaMode::Opaque,
+                view_formats: vec![wgpu::TextureFormat::Bgra8UnormSrgb],
+            },
+        );
+    }
+
+    fn render(&mut self, time: f32, surface_texture: wgpu::SurfaceTexture) {
+        let size = PhysicalSize {
+            width: surface_texture.texture.width(),
+            height: surface_texture.texture.height(),
+        };
+
+        // Run shader (on CPU)
+        self.cpu_buffer
+            .resize(size.width as usize * size.height as usize, [0; 4]);
+        run_fragment_shader(time, &mut self.cpu_buffer, size);
+
+        // Copy data from CPU memory to GPU memory.
+        //
+        // TODO: It would be more efficient to use write_buffer_with() and a texture copy command.
+        self.queue.write_texture(
+            wgpu::TexelCopyTextureInfo {
+                texture: &surface_texture.texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
+            },
+            self.cpu_buffer.as_flattened(),
+            wgpu::TexelCopyBufferLayout {
+                offset: 0,
+                bytes_per_row: Some(size.width * 4),
+                rows_per_image: None,
+            },
+            wgpu::Extent3d {
+                width: size.width,
+                height: size.height,
+                depth_or_array_layers: 1,
+            },
+        );
+
+        // write_texture() is deferred to the next submit. Usually, this doesn’t matter because
+        // there is at least one command submitted, such as a render pass, that uses the written
+        // data, but in this case we are writing directly to the surface texture, so we need a
+        // dummy submit to flush the write.
+        self.queue.submit([]);
+
+        surface_texture.present();
     }
 }
